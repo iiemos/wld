e66cc46decd356da82be8da4e37170e6550f7822
ason <2254749998@qq.com>
2024-01-27 12:45:23 +0800

    123

--- src/store.js
+++ src/store.js
@@ -51,7 +51,7 @@ export const useGlobalState = createGlobalState(
     const FomoBalance = ref(0)
     const userLevel = ref('0')
     const inviteLink = ref('0x76a569aBA8E6909e9350A36389D0a061e1264193')
-    const contractAddress=ref('0x7da937415B5963c0f91326Dd6ab9674e4547de1f')// Defi合约地址
+    const contractAddress=ref('0xE60e8BC55D2931a7a9587F245dbB21fDc88EaB66')// Defi合约地址
 
     // const Router_ADDRESS=ref('0x10ED43C718714eb63d5aA57B78B54704E256024E') // 主网薄饼V2路由合约地址
     const Router_ADDRESS=ref('0xBA6b07C1cDE52cC51ff7357ECc43A47f9581a291') //  测试路由合约地址
